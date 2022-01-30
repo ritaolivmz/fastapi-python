728029fd00318209c8e2bb957ee6047948210f2b
ritaolivmz <ritaolivmz@gmail.com>
2022-01-30 21:21:50 +0100

    Update main.py
    Path Parameters: Get an Item by ID

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+#Path Parameters: Get an Item by ID
+dict = {"obj1":"val1", "obj2":"val2"}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id:str):
+    return dict[item_id]
