2df18f6464c1ff7a24c48b7ab81d32cae6cf8d22
ritaolivmz <ritaolivmz@gmail.com>
2022-01-31 09:13:06 +0100

    Update main.py
    POST operation with exception handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,3 +13,20 @@ dict = {"obj1":"val1", "obj2":"val2"}
 @app.get("/items/{item_id}")
 async def get_item(item_id:str):
     return dict[item_id]
+
+#POST operation
+
+class Book(BaseModel):
+    isbn: int
+    title: str
+
+books = []
+
+@app.post("/books/")
+def create_book(book: Book) -> Book:
+    print("got here")
+    if book in books:
+        raise HTTPException(status_code=409, detail=f'Book with isbn {book.isbn} already exists')
+    books.append(book)
+    return book
+
