b57a470554547714f9eeb66d19a6c96b803004c2
ritaolivmz <ritaolivmz@gmail.com>
2022-01-31 09:47:37 +0100

    Update main.py
    POST item

--- main.py
+++ main.py
@@ -8,11 +8,28 @@ async def root():
     return {"message": "Hello World"}
 
 #Path Parameters: Get an Item by ID
-dict = {"obj1":"val1", "obj2":"val2"}
+
+class Item(BaseModel):
+    item_id: str
+    item_value: str
+
+dict = {}
 
 @app.get("/items/{item_id}")
 async def get_item(item_id:str):
+    if item_id not in dict:
+        raise HTTPException(status_code=409, detail=f'Item with id {item_id} doesn''t exist')
     return dict[item_id]
+
+#POST item
+@app.post("/items/")
+def create_item(item: Item) -> Item:
+    print("got here")
+    if item.item_id in dict:
+        raise HTTPException(status_code=409, detail=f'Item with id {item.item_id} already exists')
+    dict[item.item_id] = item
+    return item
+
 
 #POST operation
 
@@ -24,7 +41,6 @@ books = []
 
 @app.post("/books/")
 def create_book(book: Book) -> Book:
-    print("got here")
     if book in books:
         raise HTTPException(status_code=409, detail=f'Book with isbn {book.isbn} already exists')
     books.append(book)
